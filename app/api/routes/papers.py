1aea80192e6e9b94ea06cf9bde4754f7c0f0d11e
Billy.C <72890070+BillyCherres@users.noreply.github.com>
2026-03-10 12:22:28 -0500

    allow upload of pdfs

--- app/api/routes/papers.py
+++ app/api/routes/papers.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, File, HTTPException, UploadFile
 
 from app.schemas.paper import PaperCreate, PaperResponse
 from app.services.paper_service import createPaper, get_all_papers, get_paper_by_id
 
 from typing import List
+
+from app.services.pdf_service import extract_text_from_pdf
 
 
 router = APIRouter()
@@ -24,3 +26,25 @@ def get_paper_by_id_endpoint(id: int):
         raise HTTPException(status_code=404, detail="Paper not found")
 
     return paper
+
+@router.post("/papers/upload", response_model=PaperResponse)
+def post_paper(file: UploadFile = File(...)):
+    if file.content_type != "application/pdf":
+        raise HTTPException(status_code=400, detail="File must be a PDF")
+    
+    extracted_text = extract_text_from_pdf(file.file)
+    
+    if not extracted_text:
+        raise HTTPException(status_code=400, detail="Could not extract text from PDF")
+
+    paper_data = PaperCreate(
+        title = file.filename.replace(".pdf", ""),
+        authors= [],
+        abstract= "",
+        content= extracted_text
+    )
+    
+    created_paper = createPaper(paper_data)
+    
+    return created_paper
+
